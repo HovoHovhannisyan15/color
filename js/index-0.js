bb8c749847b273d140496c6cbf8be79de5f244ed
Hovhannes <it.engineer.95@mail.ru>
2024-05-29 18:20:37 +0300

    first commit

---
+++ js/index-0.js
@@ -0,0 +1,15 @@
+// console.log(window.innerWidth)
+// console.log(window.innerHeight)
+
+// window.open("about:blank", "zagalovok", "width=200,height=200")
+
+// console.log(navigator.userAgent);
+// console.log(navigator.platform);
+
+// console.log(location.href);
+
+// setTimeout(function() {
+//     location.reload();
+// }, 5000);
+
+// location.href = "https://itproger.com/"
---
+++ js/index-1.js
@@ -0,0 +1,38 @@
+
+
+let block = document.querySelector('div.block');
+
+function showBlock() {
+    block.style.display = "flex"
+};
+function hideBlock() {
+    block.style.display = "none"
+};
+
+
+let list = document.querySelector('ul.listColor');
+
+function showList() {
+    list.style.display = "flex"
+};
+function hideList() {
+    list.style.display = "none"
+};
+
+// Color Yellow
+function yellow() {
+    block.style.backgroundColor = "yellow";
+}
+function green() {
+    block.style.backgroundColor = "green";
+}
+function greenyellow() {
+    block.style.backgroundColor = "greenyellow";
+}
+function red() {
+    block.style.backgroundColor = "red";
+}
+function white() {
+    block.style.backgroundColor = "white";
+}
+
